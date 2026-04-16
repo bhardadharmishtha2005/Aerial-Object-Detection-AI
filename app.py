fb6508af0e2ccd88f90a34c60fc7498c9f925676
Dharmishtha Bharda <bhardadharmishtha2005@gmail.com>
2026-04-16 16:07:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pandas as pd
 import os
 
 # --- PAGE CONFIG ---
-st.set_page_config(page_title="SkyGuard: Aerial Intelligence", page_icon="🛡️", layout="wide")
+st.set_page_config(page_title="Aerial Object Detection Intelligence", page_icon="🛡️", layout="wide")
 
 # --- CUSTOM CSS ---
 st.markdown("""<style>#MainMenu {visibility: hidden;} footer {visibility: hidden;} header {visibility: hidden;}</style>""", unsafe_allow_html=True)
